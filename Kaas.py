e1a71fda6a0d586a02ce1dd46942023611174088
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-09-28 09:51:14 +0200

    verbetering

--- Kaas.py
+++ Kaas.py
@@ -1,30 +1,30 @@
-kaas_geel = input(str('Is de kaas geel?: '))
-if kaas_geel == 'ja':
-    kaas_gat = input(str('Zitten er gaten in?: '))
-    if kaas_gat == 'ja':
-        kaas_duur = input(str('Is de kaas belachelijk duur?: '))
-        if kaas_duur == 'ja':
+kaasGeel = input(str('Is de kaas geel?: '))
+if kaasGeel == 'ja':
+    kaasGat = input(str('Zitten er gaten in?: '))
+    if kaasGat == 'ja':
+        kaasDuur = input(str('Is de kaas belachelijk duur?: '))
+        if kaasDuur == 'ja':
             print('De kaas is Emmenthaler. ')
-        if kaas_duur == 'nee':
+        if kaasDuur == 'nee':
             print('De kaas is Leerdammer')
-    if kaas_gat == 'nee':
-        steen_kaas = input(str('Is de kaas hard als steen?: '))
-        if steen_kaas == 'ja':
+    if kaasGat == 'nee':
+        steenKaas = input(str('Is de kaas hard als steen?: '))
+        if steenKaas == 'ja':
             print('De kaas is Parmigiano Reggiano')
-        if steen_kaas == 'nee':
+        if steenKaas == 'nee':
             print('De kaas is Goudse kaas')
 
-if kaas_geel == 'nee':
-    schimmel_kaas = input(str('Heeft de kaas blauwe schimmels?: '))
-    if schimmel_kaas == 'ja':
-        korst_kaas = input(str('Heeft de kaas een korst?: '))
-        if korst_kaas == 'ja':
+if kaasGeel == 'nee':
+    schimmelKaas = input(str('Heeft de kaas blauwe schimmels?: '))
+    if schimmelKaas == 'ja':
+        korstKaas = input(str('Heeft de kaas een korst?: '))
+        if korstKaas == 'ja':
             print('De kaas is Bleu de Rochbaron')
-        if korst_kaas == 'nee':
+        if korstKaas == 'nee':
             print('De kaas is Fourme d\'Abert')
-    if schimmel_kaas == 'nee':
-        korst_kaas2 = input(str('Heeft de kaas een korst?: '))
-        if korst_kaas2 == 'ja':
+    if schimmelKaas == 'nee':
+        korstKaas2 = input(str('Heeft de kaas een korst?: '))
+        if korstKaas2 == 'ja':
             print('De kaas is Camambert')
-        if korst_kaas2 == 'nee':
+        if korstKaas2 == 'nee':
             print('De kaas is Mozzarella')
